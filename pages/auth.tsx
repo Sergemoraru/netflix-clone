40674f7ff1cd65247b45afd4e9c19c43f76bfb37
Serge Moraru <sergemoraru@gmail.com>
2023-07-02 16:37:09 -0400

    Added prisma, register and auth.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,12 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
 import Input from "@/components/input"
-import { SetStateAction, useState } from "react"
+import { SetStateAction, useCallback, useState } from "react"
+import axios from "axios"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/router"
+
+import { FcGoogle } from "react-icons/fc"
+import { FaGithub } from "react-icons/fa"
 
 const Auth = () => {
+    const router = useRouter()
+
     const [email, setEmail] = useState('')
     const [name, setName] = useState('')
     const [password, setPassword] = useState('')
 
-    const [varient, setVarient] = useState('login')
+    const [variant, setVariant] = useState('login')
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, [])
+
+    const login = useCallback(async () => {
+        try{
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/',
+            })
+            router.push('/')
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }, [email, password, router])
+
+    const register = useCallback(async () => {
+        try{
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            })
+            login();
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }, [email, name, password, login])
+
+    
+
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')]">
@@ -17,36 +62,48 @@ const Auth = () => {
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font font-semibold">
-                            Sign in
+                            {variant === 'login' ? 'Sign In' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
-                            <Input 
-                                label="Username"
-                                id="name"
-                                value={name}
-                                onChange={(ev: { target: { value: SetStateAction<string>} } ) => setName(ev.target.value)} type={""}                                />
+                            {variant === 'register' && (
+                                <Input 
+                                    label="Username"
+                                    id="name"
+                                    value={name}
+                                    onChange={(ev: { target: { value: SetStateAction<string>} } ) => setName(ev.target.value)}
+                                    type={""}
+                                />
+                            )}
                             <Input 
                                 label="Email"
                                 id="email"
                                 type="email"
                                 value={email}
                                 onChange={(ev: { target: { value: SetStateAction<string> } }) => setEmail(ev.target.value)}
-                                />
-                                <Input 
+                            />
+                            <Input 
                                 label="Password"
                                 id="password"
                                 type="password"
                                 value={password}
                                 onChange={(ev: { target: { value: SetStateAction<string> } }) => setPassword(ev.target.value)}
-                                />
+                            />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                             Login
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div onClick={() => signIn('google', {callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={30} />
+                            </div>
+                            <div onClick={() => signIn('github', {callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FaGithub size={30} />
+                            </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
-                            First time using Netflix? 
-                            <span className="text-white ml-1 hover:underline cursor-pointer">
-                                 Create an account
+                            {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'} 
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                                 {variant === 'login' ? 'Create an account' : 'Sign in'}
                             </span>
                         </p>
                     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    
+    <h1 className='text-white'>homepage</h1>
     </>
   )
 }
