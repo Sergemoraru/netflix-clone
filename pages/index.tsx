b9518bffd5706ac76ec0cb1a5eb5ee7357910738
Serge Moraru <sergemoraru@gmail.com>
2023-07-08 07:38:06 -0400

    Added current user, and logout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,33 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import useCurrentUser from "@/hooks/useCurrentUser"
+import { NextPage, NextPageContext } from "next"
+import { getSession, signOut } from "next-auth/react"
+import { redirect } from "next/dist/server/api-utils"
 
-const inter = Inter({ subsets: ['latin'] })
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+
+    if (!session) {
+      return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props: {}
+  }
+}
+
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-    <h1 className='text-white'>homepage</h1>
+    <h1 className='text-white'>Homepage</h1>
+    <p className="text-white ">Logged in as: {user?.name}</p>
+    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
---
+++ pages/profiles.tsx
@@ -0,0 +1,9 @@
+const profiles = () => {
+    return(
+        <div>
+            <p className="text-white text-4xl">Profiles</p>
+        </div>
+    )
+}
+
+export default profiles;
