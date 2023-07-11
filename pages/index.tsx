8b846aca78109f46370ae81b60c618e83834b39a
Serge Moraru <sergemoraru@gmail.com>
2023-07-10 20:59:53 -0400

    Prisma issue fixed. MovieCards added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Billboard from "@/components/Billboard"
+import MovieList from "@/components/MovieList"
 import Navbar from "@/components/Navbar"
-import useCurrentUser from "@/hooks/useCurrentUser"
-import { NextPage, NextPageContext } from "next"
-import { getSession, signOut } from "next-auth/react"
-import { redirect } from "next/dist/server/api-utils"
+import useMovieList from "@/hooks/useMovieList"
+import {  NextPageContext } from "next"
+import { getSession } from "next-auth/react"
+
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -19,14 +21,18 @@ export async function getServerSideProps(context: NextPageContext) {
     props: {}
   }
 }
-
+ 
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
     <Navbar />
+    <Billboard />
+    <div className="pb-40">
+      <MovieList title="Trending Now" data={movies}/>
+    </div>
     </>
   )
 }
